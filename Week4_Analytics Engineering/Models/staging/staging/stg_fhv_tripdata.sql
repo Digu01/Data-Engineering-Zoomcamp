ace65d3ce716b4894f50bee110ec1317bb651125
Digamber Deshmukh <98606505+Digu01@users.noreply.github.com>
2024-02-24 18:19:34 +0530

    Create stg_fhv_tripdata.sql
    stg_fhv_tripdata.sql

---
+++ Week4_Analytics Engineering/Models/staging/staging/stg_fhv_tripdata.sql
@@ -0,0 +1,28 @@
+{{
+    config(
+        materialized='view'
+    )
+}}
+
+select
+    -- identifiers
+    dispatching_base_num,
+    {{ dbt.safe_cast("pulocationid", api.Column.translate_type("integer")) }} as pickup_locationid,
+    {{ dbt.safe_cast("dolocationid", api.Column.translate_type("integer")) }} as dropoff_locationid,
+    
+    -- timestamps
+    cast(pickup_datetime as timestamp) as pickup_datetime,
+    cast(dropoff_datetime as timestamp) as dropoff_datetime,
+    
+    -- trip info
+    sr_flag,
+    affiliated_base_number
+from {{ source('staging','fhv_tripdata') }}
+
+
+-- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
+{% if var('is_test_run', default=true) %}
+
+  limit 100
+
+{% endif %}
